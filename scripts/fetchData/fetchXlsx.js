65e239ffdc72ee0f8883fdc0a32e3342e455cfbb
callumblanchard <callum.blanchard@hotmail.co.uk>
2020-09-11 11:20:32 +0100

    Initial commit

--- scripts/fetchData/fetchXlsx.js
+++ scripts/fetchData/fetchXlsx.js
@@ -2,21 +2,13 @@ const xlsx = require('xlsx');
 const fs = require('fs');
 const { processData } = require('./dataProcessor');
 
-// Promise.delay = (t, val) => new Promise((resolve) => {
-//   setTimeout(resolve.bind(null, val), t);
-// });
-
-// Promise.raceAll = (promises, timeoutTime, timeoutVal) => Promise.all(
-//   promises.map((p) => Promise.race([p, Promise.delay(timeoutTime, timeoutVal)])),
-// );
-
-const includeSheets = ['Data All'];
+const includeSheets = [];
 // const excludeSheets = [];
 
 function main() {
   const results = {};
 
-  const wb = xlsx.readFile('data/Talent Factories Analysis.xlsx', { type: 'buffer' });
+  const wb = xlsx.readFile('data/data.xlsx', { type: 'buffer' });
 
   wb.SheetNames.forEach((name) => {
     if (includeSheets.indexOf(name) === -1) return;
@@ -26,7 +18,11 @@ function main() {
     results[name] = xlsx.utils.sheet_to_json(ws);
   });
 
-  fs.writeFileSync('data/real.json', JSON.stringify(processData(results), null, 2));
+  if (Object.keys(results).length === 1) {
+    fs.writeFileSync('data/real.json', JSON.stringify(processData(Object.values(results)[0]), null, 2));
+  } else {
+    fs.writeFileSync('data/real.json', JSON.stringify(processData(results), null, 2));
+  }
 }
 
 main();
